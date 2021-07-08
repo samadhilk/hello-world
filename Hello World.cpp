ebe6af1c575850961f3666c61d476477a3ab659d
musicbysamadhi <69573759+musicbysamadhi@users.noreply.github.com>
2021-07-08 17:18:07 +0530

    C++ project
    C++ project file for Hello World Program

---
+++ Hello World.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "Hello World" << endl;
+    cout << "This is my first C++ program" << endl << "I'm Samadhi" << endl;
+    cout << "This\nis\nNoice!\n";
+    cout << "\n";
+    //\n is used to end a line similar to endl
+
+    cout << "Name\t:\tSamadhi\nAge\t:\t21";
+    //\t is used to add tab space
+
+    return 0;
+}
